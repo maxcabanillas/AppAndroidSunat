89f9b6582851ff7b62ccbf666f773aebb88eb04c
Manuel Gustavo Zavaleta Vargas <mzavaletavargas@gmail.com>
2015-01-25 23:39:05 -0500

    First Commit

---
+++ app/src/main/java/solodescuentos/com/app/DisplayTiendasActivity.java
@@ -0,0 +1,119 @@
+package solodescuentos.com.app;
+
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+
+public class DisplayTiendasActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_tiendas);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+
+
+
+    }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new HttpRequestTask().execute();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_display_tiendas, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_refresh) {
+            new HttpRequestTask().execute();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private class HttpRequestTask extends AsyncTask<Void, Void, Greeting> {
+        @Override
+        protected Greeting doInBackground(Void... params) {
+            try {
+                final String url = "http://restsunat.herokuapp.com/notificaciones/54b313d5e4b0aa674b25944c";
+                RestTemplate restTemplate = new RestTemplate();
+                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+                Greeting greeting = restTemplate.getForObject(url, Greeting.class);
+                return greeting;
+            } catch (Exception e) {
+                Log.e("MainActivity", e.getMessage(), e);
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Greeting greeting) {
+            TextView greetingNombre = (TextView) findViewById(R.id.nombre_content_label);
+            TextView greetingFecha = (TextView) findViewById(R.id.fecha_content_value);
+            TextView greetingUbicacion = (TextView) findViewById(R.id.ubicacion_content_value);
+            TextView greetingDestino = (TextView) findViewById(R.id.destino_content_value);
+
+            greetingNombre.setText(greeting.getNombre());
+            greetingFecha.setText(greeting.getFecha());
+            greetingUbicacion.setText(greeting.getUbicacion());
+            greetingDestino.setText(greeting.getDestino());
+
+        }
+
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_display_tiendas, container, false);
+            return rootView;
+        }
+    }
+}
---
+++ app/src/main/java/solodescuentos/com/app/Greeting.java
@@ -0,0 +1,63 @@
+package solodescuentos.com.app;
+
+/**
+ * Created by IIIII on 1/24/2015.
+ */
+public class Greeting {
+
+    private String nombre;
+    private String fecha;
+    private String ubicacion;
+    private String destino;
+    private String _id;
+    private String descripcion;
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
